c29770341806c55e224986138dd44b5335e356f1
AyaanSayed <138655396+AyaanSayed@users.noreply.github.com>
2025-07-14 17:58:47 +0530

    Add initial project setup with React, Tailwind CSS, and various assets

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
+
