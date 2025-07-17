0e2015f2dd163e6a6bac6cb0621445a9f1bed11b
AyaanSayed <138655396+AyaanSayed@users.noreply.github.com>
2025-07-17 23:08:17 +0530

    update Footer to display the current year and add Projects link to Navbar

--- client/src/sections/Footer.jsx
+++ client/src/sections/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
           </a>
         ))}
       </div>
-      <p>© 2025 Ali. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Ali. All rights reserved.</p>
     </section>
   );
 };
